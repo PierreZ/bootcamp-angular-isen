fb175637866310c2d7c4c898e90bf971d2ead988
Pierre Zemb <pierre.zemb.isen@gmail.com>
2014-02-24 14:38:29 +0100

    adding step 6

---
+++ step-6/js/app.js
@@ -0,0 +1,29 @@
+var myApp = angular.module('studentApp', []);
+myApp.controller('Ctrl', function ($scope) {
+	$scope.students = new Array();
+	$scope.students.push({ 
+		"name": "Pierre Zemb",
+		"class" : "CIR3" ,
+		"text" : "Seems like a nice guy"
+	});
+	$scope.students.push({ 
+		"name": "Brendan",
+		"class" : "CIR1" ,
+		"text" : "Seems like a weird guy"
+	});
+	$scope.students.push({ 
+		"name": "yolo",
+		"class" : "CIR1" ,
+		"text" : "Seems like a weird guy"
+	});
+
+	$scope.remove = function (name){
+    	angular.forEach($scope.students, function(student,key) {
+      		if (student.name==name) {
+      			$scope.students.splice(key,1);
+      		};
+    	});
+    };
+
+	
+});
